08fb77e456bce428b843f8154848214355c1a8a2
Çağan Parlapan <parlapancagan0907@gmail.com>
2026-05-16 00:43:10 +0300

    feat(service): add ExtractionException

---
+++ src/main/java/com/iae/service/extract/ExtractionException.java
@@ -0,0 +1,12 @@
+package com.iae.service.extract;
+
+public class ExtractionException extends Exception {
+
+    public ExtractionException(String message) {
+        super(message);
+    }
+
+    public ExtractionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
